66d756ba5ab0811e11b50b643c31eab93cdc3a1a
CY Kalu <cy23@tutanota.com>
2021-01-01 12:16:41 +0100

    Seed db using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do
+  user = User.new
+  user.name = Faker::Name.name
+  user.email = Faker::Internet.email
+  user.save
+end
+
+10.times do
+  event = Event.new
+  event.creator = User.all.sample
+  event.name = Faker::Lorem.sentence(word_count: [5, 6, 7, 8].sample) # Faker::Book.name
+  event.date = rand(Date.today...Date.today + 2.years)
+  event.location = Faker::Address.full_address
+  event.description = Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false)
+  event.save
+end
+
+
+50.times do
+  event = Event.all.sample
+  user = User.all.sample
+
+  if !event.attendees.include?(user)
+    event.attendees << user
+  end
+end
