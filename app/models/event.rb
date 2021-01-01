96b65bfe12489c6131ec2f6129fee792e911bacd
CY Kalu <cy23@tutanota.com>
2021-01-01 12:01:19 +0100

    Destroy invitations when an event is destroyed

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
-  has_many :invitations
+  has_many :invitations, dependent: :destroy
   has_many :attendees, through: :invitations, source: :attendee
 
   validates :name, :description, :location, :date, :creator_id, presence: true
