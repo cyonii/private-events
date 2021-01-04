e0d841062975002576910d8f7ed5efdb4af855ed
CY Kalu <cy23@tutanota.com>
2021-01-04 09:02:38 +0100

    Remove unnecessary code

--- db/migrate/20201231212003_create_invitations.rb
+++ db/migrate/20201231212003_create_invitations.rb
@@ -1,8 +1,8 @@
 class CreateInvitations < ActiveRecord::Migration[6.0]
   def change
     create_table :invitations do |t|
-      t.references :event, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.references :event
+      t.references :user
 
       t.timestamps
     end
